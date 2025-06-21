0685fd5326e534830737af188e5a537b29b884b0
dmosc <osdavrm@gmail.com>
2025-06-21 06:03:40 -0500

    Escape `'` charaters using &apos;.

--- components/custom/overview.tsx
+++ components/custom/overview.tsx
@@ -12,7 +12,7 @@ export const Overview = () => {
     >
       <div className="p-12 flex flex-col gap-3 leading-relaxed text-center max-w-xl">
         <p className="flex flex-row justify-center items-center text-s">
-          You're odering from
+          You&apos;re odering from
         </p>
         <p className="flex flex-row justify-center items-center font-bold text-2xl">
           mealpreps
@@ -22,7 +22,7 @@ export const Overview = () => {
           doorstep.
         </p>
         <p className="mb-5 flex flex-row justify-center items-center text-s">
-          I'm a smart agent ready to help you place an order. I can answer any
+          I&apos;m a smart agent ready to help you place an order. I can answer any
           question about the menu, make modifications, keep track of your order
           and help you process final details like delivery instructions and
           payment.
